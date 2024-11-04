003687b9a7c62f28199b5f9a77f237a0ebbc528e
Lareine Jeng <lareine.jeng@cooper.edu>
2024-11-03 23:57:20 -0500

    Add files via upload

---
+++ lecture-pages/src.js
@@ -0,0 +1,131 @@
+const lecture_map = {
+  "A0": {
+    name: "Introduction",
+    count: 43,
+    subfolder: "",
+    code: []
+  },
+  "A1": {
+    name: "Parallel Processing Architectures",
+    count: 36,
+    subfolder: "",
+    code: []
+  },
+  "A2": {
+    name: "Large Scale Processing on the Cloud",
+    count: 33,
+    subfolder: "",
+    code: []
+  },
+  "A3": {
+    name: "Large Scale Processing on the Cloud",
+    count: 41,
+    subfolder: "",
+    code: []
+  },
+  "A4": {
+    name: "Application Parallelism",
+    count: 40,
+    subfolder: "",
+    code: []
+  },
+  "A5": {
+    name: "Designing Parallel Programs",
+    count: 35,
+    subfolder: "",
+    code: []
+  },
+  "B1": {
+    name: "Foundations of Parallel Computing",
+    count: 40,
+    subfolder: "",
+    code: []
+  },
+  "H1": {
+    name: "Hands-on",
+    count: 30,
+    subfolder: "code",
+    code: [
+      "example1.py", "example2.py", "example3.py", "example4.py", "example5.py",
+      "example6.py", "example7.py", "example8.py", "example9.py", "pi1.py", "pi2.py"
+    ]
+  },
+  "B2": {
+    name: "Performance Optimization",
+    count: 45,
+    subfolder: "",
+    code: []
+  },
+  "B3": {
+    name: "Accelerated Computing",
+    count: 55,
+    subfolder: "",
+    code: []
+  },
+  "B4": {
+    name: "Shared-Memory Parallel Processing",
+    count: 50,
+    subfolder: "",
+    code: []
+  },
+  "H2": {
+    name: "Hands-on",
+    count: 49,
+    subfolder: "",
+    code: []
+  },
+  "B5": {
+    name: "Distributed-Memory Parallel Processing",
+    count: 41,
+    subfolder: "",
+    code: []
+  },
+  "H3": {
+    name: "Hands-on",
+    count: 47,
+    subfolder: "",
+    code: []
+  },
+  "C1": {
+    name: "Batch Data Processing",
+    count: 33,
+    subfolder: "code",
+    code: ["reducer.py", "mapper.py", "counter.py", "input.txt"]
+  },
+  "H4": {
+    name: "Hands-on",
+    count: 28,
+    subfolder: "src",
+    code: ["reducer.py", "mapper.py", "dat.zip", "solutions/reducer_ii.py", "solutions/mapper_ii.py"]
+  },
+  "C2": {
+    name: "Dataflow Processing",
+    count: 27,
+    subfolder: "",
+    code: []
+  },
+  "H5": {
+    name: "Hands-on",
+    count: 27,
+    subfolder: "code",
+    code: ["input.txt"]
+  },
+  "C3": {
+    name: "Stream Data Processing",
+    count: 37,
+    subfolder: "code",
+    code: ["network_wordcount.py", "twitter_client.py", "tweet_processing.py"]
+  },
+  "G1": {
+    name: "Graph Parallel Processing",
+    count: 25,
+    subfolder: "",
+    code: []
+  },
+  "wrapup": {
+    name: "Wrapup Lecture",
+    count: 34,
+    subfolder: "",
+    code: []
+  }
+};
